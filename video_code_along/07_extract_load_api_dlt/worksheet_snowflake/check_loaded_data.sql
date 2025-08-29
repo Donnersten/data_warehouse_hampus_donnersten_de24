637dc2e220ae89468530914c96c555d800253e59
Donnersten <hampus@donnersten.se>
2025-08-29 09:35:59 +0200

    07 video code along done

---
+++ video_code_along/07_extract_load_api_dlt/worksheet_snowflake/check_loaded_data.sql
@@ -0,0 +1,14 @@
+USE ROLE job_ads_dlt_role;
+
+USE DATABASE job_ads;
+
+SHOW SCHEMAS;
+
+SHOW TABLES IN SCHEMA staging;
+
+DESC TABLE staging.data_field_job_ads;
+
+USE WAREHOUSE dev_wh;
+SELECT HEADLINE, EMPLOYER__WORKPLACE FROM staging.data_field_job_ads;
+
+SELECT * FROM staging.data_field_job_ads
